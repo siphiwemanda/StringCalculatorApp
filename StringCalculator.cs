1bc1a5c031bda83db689edb8dcbb61708d50f13f
Siphiwe <siphiwemanda@gmail.com>
2020-11-23 08:59:33 +0000

    first commit

---
+++ StringCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculatorApp
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+           
+            if (HasCustomSeparator(numbers))
+            {
+                return Sum(GetNumbersString(numbers), GetCustomSeparator(numbers));
+            }
+
+            return Sum(numbers, new[] {",", "\n"});
+        }
+
+        public string GetNumbersString(string definition)
+        {
+            return definition.Substring(FindEndOfDefinition(definition) + 1);
+        }
+
+        private int FindEndOfDefinition(string definition){return definition.IndexOf('\n');}
+
+        public string[] GetCustomSeparator(string definition)
+        {
+            const int startOfDefinition = 2;
+
+            var customSeparator =
+                definition.Substring(startOfDefinition, FindEndOfDefinition(definition) - startOfDefinition);
+            customSeparator = customSeparator.Replace("[", string.Empty)
+                                            .Replace("]", string.Empty);
+
+            return new[] {customSeparator};
+        }
+
+        private bool HasCustomSeparator(string numbers)
+        {
+          
+            return numbers.StartsWith("//");
+        }
+
+        private int Sum(string numbers, string[] separator)
+        {
+            var actualNumbers = RemoveNumbersOverThousand(SplitStringsIntoNumbers(numbers, separator));
+            CheckForNegativeNumbers(actualNumbers);
+
+            return actualNumbers.Sum(); 
+        }
+
+        private void CheckForNegativeNumbers(int[] actualNumbers)
+        {
+            if (actualNumbers.Any(i => i < 0))
+            {
+                throw new Exception($"negatives not allowed: {string.Join(",", actualNumbers.Where(i => i < 0))}");
+            }
+        }
+
+        private IEnumerable<int> SplitStringsIntoNumbers(string numbers, string[] separator)
+        {
+            var splitNumbers = numbers.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            return splitNumbers.Select(int.Parse);
+        }
+
+        private int[] RemoveNumbersOverThousand(IEnumerable<int> actualNumbers)
+        {
+             return actualNumbers.Where(i => i < 1001).ToArray();
+           
+        }
+    }
+}
